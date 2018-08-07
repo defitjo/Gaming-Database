2090ddd288965b67f169fee65e51909f401d2d17
Devon <defitjo16@gmail.com>
2018-08-06 20:30:28 -0400

    schema and app

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -17,6 +17,26 @@ const GameType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
     date: { type: GraphQLString },
+    developer: {
+      type: DeveloperType,
+      resolve(parent, args) {
+        return Developer.findById(parent.developerId);
+      },
+    },
+  }),
+});
+
+const DeveloperType = new GraphQLObjectType({
+  name: 'Developer',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    games: {
+      type: new GraphQLList(GameType),
+      resolve(parent, args) {
+        return Game.find({ developerId: parent.id });
+      },
+    },
   }),
 });
 
@@ -28,10 +48,17 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return Game.findById(args.id);
-      }
-    }
-  }
-})
+      },
+    },
+    developer: {
+      type: DeveloperType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Developer.findById(args.id);
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
